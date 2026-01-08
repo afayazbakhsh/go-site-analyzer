1d176773d0be61f39156d88efc2def81b97ff5b2
Amir Fayazbakhsh <a.fayazbakhsh1@gmail.com>
2026-01-08 16:01:31 +0330

    refactor: change routes

--- app/httpserver/routes.go
+++ app/httpserver/routes.go
@@ -1,21 +1,27 @@
 package httpserver
 
 import (
+	"gocrawler/app/httpserver/controllers"
 	"net/http"
 
 	"github.com/gin-gonic/gin"
 )
 
-func RegisterRoutes(r *gin.Engine) {
+func RegisterRoutes(route *gin.Engine) {
 
-	r.GET("/", func(c *gin.Context) {
+	route.GET("/", func(c *gin.Context) {
 		c.JSON(http.StatusOK, gin.H{
 			"message": "Hello, Gin is running!",
 		})
 	})
 
-	api := r.Group("/api/v1")
+	api := route.Group("/api/v1/")
 	{
-		api.GET("/check", CheckHandler) // call handler in handler.go
+		api.GET("check", controllers.CheckHandler) // call handler in handler.go
+
+		api.Group("page-data/")
+		{
+			api.POST("create", controllers.CreatePageData)
+		}
 	}
 }
